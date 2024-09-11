6a1c24704987a2f07fb4c94f97b241043fca0b22
jgyb89 <james.green335@du.edu>
2024-09-10 20:40:14 -0400

    Initial commit

---
+++ src/components/Collaboration.jsx
@@ -0,0 +1,13 @@
+import Section from "./Section";
+
+const Collaboration = () => {
+  return (
+    <Section crosses>
+      <div className="container lg:flex">
+        <div className="max-w-[25rem]"></div>
+      </div>
+    </Section>
+  );
+};
+
+export default Collaboration;
